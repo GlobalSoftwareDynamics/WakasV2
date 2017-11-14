762f36b7354672b15b5c2aa780aba827c3f213e5
JoaquinRiveraLopez <joaquin.rivera@gsdynamics.com>
2017-11-13 19:01:39 -0500

    Se arreglo el boton de Cerrar Sesion y se elimino la clase fixed-top de la navbar

--- navbarAdmin.php
+++ navbarAdmin.php
@@ -1,6 +1,6 @@
 <header class="container-fluid bg-light">
     <section class="container">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#"><img src="img/WakasNavbar.png" height="60" width="auto"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -57,7 +57,7 @@
                         </div>
                     </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0" action="index.php" method="post">
+                <form class="form-inline my-2 my-lg-0 ml-1" action="index.php" method="post">
                     <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Cerrar Sesión</button>
                 </form>
             </div>
