36e031ef49c20603199dba628d4038356457a14a
IgnacioRondon <ignacio.rondon@gsdynamics.com>
2017-11-13 22:03:00 -0500

    Se agrego el Dashboard y se modifico la barra de navegacion para dispositivos moviles

--- navbarAdmin.php
+++ navbarAdmin.php
@@ -1,6 +1,6 @@
 <header class="container-fluid bg-light">
     <section class="container">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#"><img src="img/WakasNavbar.png" height="60" width="auto"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
