eadce2d27ae76189816651e4c0652c1ccda135f4
JoaquinRiveraLopez <joaquin.rivera@gsdynamics.com>
2017-11-14 15:01:15 -0500

    Add TipoProducto en nuevaHE

--- nuevaHE.php
+++ nuevaHE.php
@@ -6,8 +6,16 @@ if(isset($_SESSION['login'])){
 	include('funciones.php');
 
 	$idProd = idgen('P');
-	?>
 
+	if(isset($_POST['addTipoProducto'])){
+		$insert = mysqli_query($link, "INSERT INTO TipoProducto(descripcion,tamanoLote) VALUES ('{$_POST['descripcionTipoProducto']}','{$_POST['tamanoLote']}')");
+
+		$queryPerformed = "INSERT INTO TipoProducto VALUES ({$_POST['descripcionTipoProducto']},{$_POST['tamanoLote']})";
+
+		$databaseLog = mysqli_query($link, "INSERT INTO DatabaseLog (idEmpleado,fechaHora,evento,tipo,consulta) VALUES ('{$_SESSION['user']}','{$date}','INSERT TIPO PRODUCTO','INSERT','{$queryPerformed}')");
+	}
+	?>
+    <form method="post" id="formProducto" action="#">
 	<section class="container">
 		<div class="row">
 			<div class="col-12">
@@ -19,7 +27,7 @@ if(isset($_SESSION['login'])){
 						</div>
 						<div class="float-right">
 							<div class="dropdown">
-								<button name="addProducto" type="submit" form="formProducto" class="btn btn-light btn-sm" formaction="nuevaHE2.php">Guardar</button>
+								<input name="addProducto" type="submit" form="formProducto" class="btn btn-light btn-sm" formaction="nuevaHE2.php" value="Guardar">
 							</div>
 						</div>
 					</div>
@@ -37,7 +45,6 @@ if(isset($_SESSION['login'])){
 									<a class="nav-link" data-toggle="tab" href="#componentes" role="tab">Componentes, Partes y Materiales</a>
 								</li>
 							</ul>
-							<form method="post" id="formProducto">
 								<input type="hidden" name="idProducto" value="<?php echo $idProd;?>">
 								<div class="tab-content">
 									<div class="tab-pane active" id="general" role="tabpanel">
@@ -45,13 +52,13 @@ if(isset($_SESSION['login'])){
 										<div class="form-group row">
 											<label for="idCorto" class="col-2 col-form-label">ID Producto:</label>
 											<div class="col-10">
-												<input class="form-control" type="text" id="idCorto" name="idCorto" required>
+												<input class="form-control" type="text" id="idCorto" name="idCorto" value="<?php if(isset($_POST['idCorto'])){echo $_POST['idCorto'];}?>">
 											</div>
 										</div>
 										<div class="form-group row">
 											<label for="idProvisional" class="col-2 col-form-label">ID Provisional Cliente:</label>
 											<div class="col-10">
-												<input class="form-control" type="text" id="idProvisional" name="idProvisional">
+												<input class="form-control" type="text" id="idProvisional" name="idProvisional" value="<?php if(isset($_POST['idProvisional'])){echo $_POST['idProvisional'];}?>">
 											</div>
 										</div>
 
@@ -59,11 +66,19 @@ if(isset($_SESSION['login'])){
 											<label for="genero" class="col-2 col-form-label">Género:</label>
 											<div class="col-6">
 												<select class="form-control" name="genero" id="genero">
-													<option disabled selected>Seleccionar</option>
-													<?php
+                                                    <?php
+                                                    if(isset($_POST['genero'])){
+	                                                    $query = mysqli_query($link,"SELECT * FROM Genero WHERE idGenero = {$_POST['genero']}");
+	                                                    while($row = mysqli_fetch_array($query)){
+		                                                    echo "<option selected value='{$row['idgenero']}'>{$row['descripcion']}</option>";
+	                                                    }
+                                                    }else{
+                                                        echo "<option disabled selected>Seleccionar</option>";
+                                                    }
+
 													$query = mysqli_query($link,"SELECT * FROM Genero");
 													while($row = mysqli_fetch_array($query)){
-														echo "<option value='{$row['idTipoProducto']}'>{$row['descripcion']}</option>";
+														echo "<option value='{$row['idgenero']}'>{$row['descripcion']}</option>";
 													}
 													?>
 												</select>
@@ -73,8 +88,16 @@ if(isset($_SESSION['login'])){
 											<label for="tipoProducto" class="col-2 col-form-label">Tipo de Producto:</label>
 											<div class="col-6">
 												<select class="form-control" name="tipoProducto" id="tipoProducto">
-													<option disabled selected>Seleccionar</option>
 													<?php
+													if(isset($_POST['tipoProducto'])){
+														$query = mysqli_query($link,"SELECT * FROM TipoProducto WHERE idTipoProducto = {$_POST['tipoProducto']}");
+														while($row = mysqli_fetch_array($query)){
+															echo "<option selected value='{$row['idTipoProducto']}'>{$row['descripcion']}</option>";
+														}
+													}else{
+														echo "<option disabled selected>Seleccionar</option>";
+													}
+
 													$query = mysqli_query($link,"SELECT * FROM TipoProducto");
 													while($row = mysqli_fetch_array($query)){
 														echo "<option value='{$row['idTipoProducto']}'>{$row['descripcion']}</option>";
@@ -83,15 +106,23 @@ if(isset($_SESSION['login'])){
 												</select>
 											</div>
 											<div class="col-2">
-												<button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#modalCategoria">Agregar Tipo de Producto</button>
+												<button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#modalTipoProducto">Agregar Tipo de Producto</button>
 											</div>
 										</div>
 										<div class="form-group row">
 											<label for="codificacionTalla" class="col-2 col-form-label">Codificación de Talla:</label>
 											<div class="col-6">
 												<select class="form-control" name="codificacionTalla" id="codificacionTalla">
-													<option disabled selected>Seleccionar</option>
 													<?php
+													if(isset($_POST['codificacionTalla'])){
+														$query = mysqli_query($link,"SELECT * FROM codificacionTalla WHERE idcodificacionTalla = {$_POST['codificacionTalla']}");
+														while($row = mysqli_fetch_array($query)){
+															echo "<option selected value='{$row['idcodificacionTalla']}'>{$row['descripcion']}</option>";
+														}
+													}else{
+														echo "<option disabled selected>Seleccionar</option>";
+													}
+
 													$query = mysqli_query($link,"SELECT * FROM codificacionTalla");
 													while($row = mysqli_fetch_array($query)){
 														echo "<option value='{$row['idcodificacionTalla']}'>{$row['descripcion']}</option>";
@@ -100,15 +131,26 @@ if(isset($_SESSION['login'])){
 												</select>
 											</div>
 											<div class="col-2">
-												<button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#modalCategoria">Agregar Codificación de Talla</button>
+												<button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#modalCodificacion">Agregar Codificación</button>
 											</div>
+                                            <div class="col-2">
+                                                <button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#modalTalla">Agregar Tallas</button>
+                                            </div>
 										</div>
 										<div class="form-group row">
 											<label for="idCliente" class="col-2 col-form-label">Cliente:</label>
 											<div class="col-6">
 												<select class="form-control" name="idCliente" id="idCliente">
-													<option disabled selected>Seleccionar</option>
 													<?php
+													if(isset($_POST['idCliente'])){
+														$query = mysqli_query($link,"SELECT * FROM Cliente WHERE idCliente = {$_POST['idCliente']}");
+														while($row = mysqli_fetch_array($query)){
+															echo "<option selected value='{$row['idCliente']}'>{$row['nombre']}</option>";
+														}
+													}else{
+														echo "<option disabled selected>Seleccionar</option>";
+													}
+
 													$query = mysqli_query($link,"SELECT * FROM Cliente WHERE estado = 1");
 													while($row = mysqli_fetch_array($query)){
 														echo "<option value='{$row['idCliente']}'>{$row['nombre']}</option>";
@@ -117,93 +159,147 @@ if(isset($_SESSION['login'])){
 												</select>
 											</div>
 											<div class="col-2">
-												<button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#modalSubcategoria">Agregar Cliente</button>
+												<button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#modalCliente">Agregar Cliente</button>
 											</div>
 										</div>
 									</div>
 									<div class="tab-pane" id="medidas" role="tabpanel">
 										<div class="spacer20"></div>
-										<div class="form-group row">
-											<label for="stockReposicion" class="col-2 col-form-label">Stock de Reposición:</label>
-											<div class="col-10">
-												<input class="form-control" type="text" id="stockReposicion" name="stockReposicion">
-											</div>
-										</div>
+
 									</div>
 									<div class="tab-pane" id="componentes" role="tabpanel">
 										<div class="spacer20"></div>
-										<div class="form-group row">
-											<label for="unidadMedida" class="col-2 col-form-label">Unidad de Medida:</label>
-											<div class="col-10">
-												<select class="form-control" name="unidadMedida" id="unidadMedida">
-													<option disabled selected>Seleccionar</option>
-													<?php
-													$query = mysqli_query($link,"SELECT * FROM UnidadMedida");
-													while($row = mysqli_fetch_array($query)){
-														echo "<option value='{$row['idUnidadMedida']}'>{$row['descripcion']}</option>";
-													}
-													?>
-												</select>
-											</div>
-										</div>
-										<div class="form-group row">
-											<label for="color" class="col-2 col-form-label">Atributo:</label>
-											<div class="col-10">
-												<input class="form-control" type="text" id="color" name="color">
-											</div>
-										</div>
-										<div class="form-group row">
-											<label for="tamano" class="col-2 col-form-label">Tamaño:</label>
-											<div class="col-6">
-												<select class="form-control" name="tamano" id="tamano">
-													<option disabled selected>Seleccionar</option>
-													<?php
-													$query = mysqli_query($link,"SELECT * FROM Tamaño");
-													while($row = mysqli_fetch_array($query)){
-														echo "<option value='{$row['idTamaño']}'>{$row['nombre']}</option>";
-													}
-													?>
-												</select>
-											</div>
-										</div>
-										<div class="form-group row">
-											<label for="genero" class="col-2 col-form-label">Género:</label>
-											<div class="col-6">
-												<select class="form-control" name="genero" id="genero">
-													<option disabled selected>Seleccionar</option>
-													<?php
-													$query = mysqli_query($link,"SELECT * FROM Genero");
-													while($row = mysqli_fetch_array($query)){
-														echo "<option value='{$row['idGenero']}'>{$row['descripcion']}</option>";
-													}
-													?>
-												</select>
-											</div>
-										</div>
-									</div>
-									<div class="tab-pane" id="url" role="tabpanel">
-										<div class="spacer20"></div>
-										<div class="form-group row">
-											<label for="urlImagen" class="col-2 col-form-label">URL Imágen:</label>
-											<div class="col-10">
-												<input class="form-control" type="text" id="urlImagen" name="urlImagen">
-											</div>
-										</div>
-										<div class="form-group row">
-											<label for="urlProducto" class="col-2 col-form-label">URL Producto:</label>
-											<div class="col-10">
-												<input class="form-control" type="text" id="urlProducto" name="urlProducto">
-											</div>
-										</div>
+
 									</div>
 								</div>
-							</form>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</section>
+
+    <div class="modal fade" id="modalTipoProducto" tabindex="-1" role="dialog" aria-labelledby="modalTipoProducto" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Agregar Tipo de Producto</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="container-fluid">
+                            <div class="form-group row">
+                                <label class="col-form-label" for="descripcionTipoProducto">Nombre de Tipo de Producto:</label>
+                                <input type="text" name="descripcionTipoProducto" id="descripcionTipoProducto" class="form-control">
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-form-label" for="tamanoLote">Tamaño de Lote:</label>
+                                <input type="text" name="tamanoLote" id="tamanoLote" class="form-control">
+                            </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary" form="formProducto" value="Submit" name="addTipoProducto">Guardar Cambios</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="modalCodificacion" tabindex="-1" role="dialog" aria-labelledby="modalCodificacion" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Agregar Codificación de Talla</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="container-fluid">
+                            <div class="form-group row">
+                                <label class="col-form-label" for="codificacion">Nombre de Codificación:</label>
+                                <input type="text" name="codificacion" id="codificacion" class="form-control">
+                            </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary" form="formProducto" value="Submit" name="addCodificacion">Guardar Cambios</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="modalTalla" tabindex="-1" role="dialog" aria-labelledby="modalTalla" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Agregar Tallas</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="container-fluid">
+
+                            <div class="form-group row">
+                                <label class="col-form-label" for="codificacion">Seleccionar Codificación:</label>
+                                <select name="cofidicacion" id="codificacion" class="form-control">
+                                    <option selected disabled>Seleccionar</option>
+                                    <?php
+                                    $search = mysqli_query($link, "SELECT * FROM codificacionTalla");
+                                    while($index = mysqli_fetch_array($search)){
+                                        echo "<option value='{$index['idcodificacionTalla']}'>{$index['descripcion']}</option>";
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-form-label" for="talla">Sigla de Talla:</label>
+                                <input type="text" name="talla" id="talla" class="form-control">
+                            </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary" form="formProducto" value="Submit" name="addTalla">Guardar Cambios</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="modalCliente" tabindex="-1" role="dialog" aria-labelledby="modalCliente" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Agregar Cliente</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="container-fluid">
+                            <div class="form-group row">
+                                <label class="col-form-label" for="ruc">RUC/DNI:</label>
+                                <input type="text" name="ruc" id="ruc" class="form-control">
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-form-label" for="razonSocial">Razón Social:</label>
+                                <input type="text" name="razonSocial" id="razonSocial" class="form-control">
+                            </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary" form="formProducto" value="Submit" name="addCliente">Guardar Cambios</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </form>
 
 	<?php
 	include('footer.php');
