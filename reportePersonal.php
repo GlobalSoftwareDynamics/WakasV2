0ca14f8282834d5b845bd472a9951fc9133cf837
IgnacioRondon <ignacio.rondon@gsdynamics.com>
2017-12-28 18:21:32 -0500

    Se completo Reporte de Personal

--- reportePersonal.php
+++ reportePersonal.php
@@ -18,7 +18,7 @@ if(isset($_SESSION['login'])){
         <div class="card">
             <div class="card-header card-inverse card-info">
                 <i class="fa fa-line-chart"></i>
-                Reporte de Producción
+                Reporte de Personal
                 <div class="float-right">
                     <div class="dropdown">
                         <button class="btn btn-light btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -109,6 +109,24 @@ if(isset($_SESSION['login'])){
                                     </div>
                                 </div>
                             </div>
+                            <div class="col-5">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <p><b>Fecha Inicio:</b></p>
+                                    </div>
+                                    <div class="col-8">
+                                        <p><?php echo $_POST['fechaInicio'];?></p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-4">
+                                        <p><b>Fecha Fin:</b></p>
+                                    </div>
+                                    <div class="col-8">
+                                        <p><?php echo $_POST['fechaFin'];?></p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div class="spacer10"></div>
                         <div class="row">
@@ -128,18 +146,172 @@ if(isset($_SESSION['login'])){
                                     </thead>
                                     <tbody>
                                     <?php
-                                    $result = mysqli_query($link,"SELECT * FROM RegistroIngresoSalida WHERE idEmpleado = '{$_POST['idEmpleado']}'");
+                                    $tiempoTotalIntervalo = 0;
+                                    $result = mysqli_query($link,"SELECT * FROM RegistroIngresoSalida WHERE idEmpleado = '{$_POST['idEmpleado']}' AND fecha >= '{$_POST['fechaInicio']}' AND fecha <= '{$_POST['fechaFin']}'");
                                     while ($fila = mysqli_fetch_array($result)){
+                                        $a = new DateTime($fila['horaSalida']);
+                                        $b = new DateTime($fila['horaIngreso']);
+                                        $interval = $a->diff($b);
+
+                                        $tiempoTotal = $interval->format("%H:%I");
+
                                         echo "<tr>";
                                         echo "<td>{$fila['fecha']}</td>";
                                         echo "<td>{$fila['horaIngreso']}</td>";
                                         echo "<td>{$fila['horaSalida']}</td>";
-                                        echo "<td></td>";
+                                        echo "<td>{$tiempoTotal}</td>";
                                         echo "</tr>";
+
+                                        $tiempoExplode = explode(":",$tiempoTotal);
+                                        $MinutosWakas = ($tiempoExplode[0]*60)+$tiempoExplode[1];
+
+                                        $tiempoTotalIntervalo = $tiempoTotalIntervalo + $MinutosWakas;
                                     }
+                                    $HorasWakas = $tiempoTotalIntervalo/60;
+                                    $HorasWakas1 = round($HorasWakas,0,PHP_ROUND_HALF_DOWN);
+                                    $HorasWakas2 = $tiempoTotalIntervalo-($HorasWakas1*60);
+                                    $HorasFinalE = $HorasWakas1.":".$HorasWakas2
                                     ?>
+                                    <tr>
+                                        <th>Total Horas Trabajadas</th>
+                                        <td></td>
+                                        <td></td>
+                                        <td><?php echo $HorasFinalE;?> Hrs.</td>
+                                    </tr>
                                     </tbody>
                                 </table>
+                            </div>
+                        </div>
+                        <div class="spacer10"></div>
+                        <div class="row">
+                            <div class="col-12">
+                                <h6 class="text-left"><b>Registro de Actividades Realizadas:</b></h6>
+                            </div>
+                            <div class="spacer10"></div>
+                            <div class="col-12" style="height: 400px; overflow-y: auto">
+                                <table class="table text-center">
+                                    <thead>
+                                    <tr>
+                                        <th>Fecha y Hora</th>
+                                        <th>Lote</th>
+                                        <th>Componente</th>
+                                        <th>Procedimiento</th>
+                                        <th>Cantidad</th>
+                                        <th>Tiempo Estándar (min)</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php
+                                    $tiempoTotalTrabajado = 0;
+                                    $result = mysqli_query($link,"SELECT * FROM EmpleadoLote WHERE idEmpleado = '{$_POST['idEmpleado']}' AND fecha >= '{$_POST['fechaInicio']}' AND fecha <= '{$_POST['fechaFin']}' ORDER BY fecha DESC");
+                                    while ($fila = mysqli_fetch_array($result)){
+                                        $result1 = mysqli_query($link,"SELECT * FROM SubProceso WHERE idProcedimiento = '{$fila['idProcedimiento']}'");
+                                        while ($fila1 = mysqli_fetch_array($result1)){
+                                            $procedimiento = $fila1['descripcion'];
+                                        }
+                                        $result1 = mysqli_query($link,"SELECT * FROM ComponentesPrenda WHERE idComponente IN (SELECT idComponente FROM ProductoComponentesPrenda WHERE idComponenteEspecifico = '{$fila['idComponenteEspecifico']}')");
+                                        while ($fila1 = mysqli_fetch_array($result1)){
+                                            $componente = $fila1['descripcion'];
+                                        }
+                                        $result1 = mysqli_query($link,"SELECT * FROM PCPSPC WHERE idComponenteEspecifico = '{$fila['idComponenteEspecifico']}' AND idSubProcesoCaracteristica IN (SELECT idSubProcesoCaracteristica FROM SubProcesoCaracteristica WHERE idProcedimiento = '{$fila['idProcedimiento']}' AND idCaracteristica = '7')");
+                                        while ($fila1 = mysqli_fetch_array($result1)){
+                                            $tiempo = $fila1['valor'];
+                                        }
+                                        echo "<tr>";
+                                        echo "<td>{$fila['fecha']}</td>";
+                                        echo "<td>{$fila['idLote']}</td>";
+                                        echo "<td>{$componente}</td>";
+                                        echo "<td>{$procedimiento}</td>";
+                                        echo "<td>{$fila['cantidad']}</td>";
+                                        echo "<td>{$tiempo} min</td>";
+                                        echo "</tr>";
+
+                                        $tiempoComponente = $fila['cantidad'] * $tiempo;
+                                        $tiempoTotalTrabajado = $tiempoTotalTrabajado + $tiempoComponente;
+                                    }
+                                    $HorasWakas3 = $tiempoTotalTrabajado/60;
+                                    $HorasWakas4 = round($HorasWakas3,0,PHP_ROUND_HALF_DOWN);
+                                    $HorasWakas5 = $tiempoTotalTrabajado-($HorasWakas4*60);
+                                    $HorasFinalT = $HorasWakas4.":".$HorasWakas5
+                                    ?>
+                                    <tr>
+                                        <th>Total</th>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td><?php echo $HorasFinalT?> Hrs.</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="spacer20"></div>
+                        <div class="row">
+                            <div class="col-12">
+                                <h6 class="text-left"><b>Registro de Tiempo Muerto:</b></h6>
+                            </div>
+                            <div class="spacer10"></div>
+                            <div class="col-12" style="height: 350px; overflow: auto">
+                                <table class="table text-center">
+                                    <thead>
+                                    <tr>
+                                        <th>Fecha</th>
+                                        <th>Actividad Muerta</th>
+                                        <th>Descripción</th>
+                                        <th>Tiempo</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php
+                                    $tiempoTotalActividadMuerta = 0;
+                                    $result = mysqli_query($link,"SELECT * FROM EmpleadoActividadMuerta WHERE idEmpleado = '{$_POST['idEmpleado']}' AND fecha >= '{$_POST['fechaInicio']}' AND fecha <= '{$_POST['fechaFin']}'");
+                                    while ($fila = mysqli_fetch_array($result)){
+                                        $result1 = mysqli_query($link,"SELECT * FROM ActividadMuerta WHERE idActividadMuerta = '{$fila['idActividadMuerta']}'");
+                                        while ($fila1 = mysqli_fetch_array($result1)){
+                                            $actvidadMuerta = $fila1['descripcion'];
+                                        }
+                                        echo "<tr>";
+                                        echo "<td>{$fila['fecha']}</td>";
+                                        echo "<td>{$actvidadMuerta}</td>";
+                                        echo "<td>{$fila['descripcion']}</td>";
+                                        echo "<td>{$fila['tiempo']}</td>";
+                                        echo "</tr>";
+
+                                        $tiempoTotalActividadMuerta = $tiempoTotalActividadMuerta + $fila['tiempo'];
+                                    }
+                                    $HorasWakas6 = $tiempoTotalActividadMuerta/60;
+                                    $HorasWakas7 = round($HorasWakas6,0,PHP_ROUND_HALF_DOWN);
+                                    $HorasWakas8 = $tiempoTotalActividadMuerta-($HorasWakas7*60);
+                                    $HorasFinalAM = $HorasWakas7.":".$HorasWakas8
+                                    ?>
+                                    <tr>
+                                        <th>Total Horas Muertas</th>
+                                        <td></td>
+                                        <td></td>
+                                        <td><?php echo $HorasFinalAM;?> Hrs.</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="spacer10"></div>
+                        <div class="row">
+                            <div class="col-12">
+                                <?php
+                                $MinutosFinalEstancia = explode(":",$HorasFinalE);
+                                $MinutosFinalEstancia = ($MinutosFinalEstancia[0]*60) + $MinutosFinalEstancia[1];
+
+                                $MinutosFinalTrabajo = explode(":",$HorasFinalT);
+                                $MinutosFinalTrabajo = ($MinutosFinalTrabajo[0]*60) + $MinutosFinalTrabajo[1];
+
+                                $MinutosFinalActividadMuerta = explode(":",$HorasFinalAM);
+                                $MinutosFinalActividadMuerta = ($MinutosFinalActividadMuerta[0]*60) + $MinutosFinalActividadMuerta[1];
+
+                                $productividad =(($MinutosFinalTrabajo-$MinutosFinalActividadMuerta)/$MinutosFinalEstancia)*100;
+                                $productividad = round($productividad,2);
+                                ?>
+                                <h6 class="text-left"><b>% Referencial de Productividad de Trabajador: </b><?php echo $productividad;?> %</h6>
                             </div>
                         </div>
                         <?php
