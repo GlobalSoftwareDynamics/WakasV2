f7db03b96a97d0d2f6e6da556997232a10572b01
IgnacioRondon <ignacio.rondon@gsdynamics.com>
2018-05-14 00:54:08 -0500

    Add files via upload

---
+++ mpdf/ttfontdata/chiller.mtx.php
@@ -0,0 +1,26 @@
+<?php
+$name='Chiller-Regular';
+$type='TTF';
+$desc=array (
+  'Ascent' => 849,
+  'Descent' => -305,
+  'CapHeight' => 849,
+  'Flags' => 4,
+  'FontBBox' => '[-118 -305 1045 849]',
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 500,
+);
+$up=-154;
+$ut=50;
+$ttffile='C:/xampp/htdocs/WakasV1/lib/mpdf/ttfonts/Chiller.ttf';
+$TTCfontID='0';
+$originalsize=118067;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='chiller';
+$panose=' a f 4 2 4 4 3 10 7 2 6 2';
+$haskerninfo=false;
+$unAGlyphs=false;
+?>
---
+++ mpdf/ttfontdata/freestylescript.mtx.php
@@ -0,0 +1,26 @@
+<?php
+$name='FreestyleScript-Regular';
+$type='TTF';
+$desc=array (
+  'Ascent' => 851,
+  'Descent' => -327,
+  'CapHeight' => 851,
+  'Flags' => 4,
+  'FontBBox' => '[-268 -327 849 851]',
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 250,
+);
+$up=-97;
+$ut=50;
+$ttffile='C:/xampp/htdocs/WakasV1/lib/mpdf/ttfonts/Freestyle Script.ttf';
+$TTCfontID='0';
+$originalsize=91692;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='freestylescript';
+$panose=' a 4 3 8 4 2 3 2 5 b 4 4';
+$haskerninfo=false;
+$unAGlyphs=false;
+?>
